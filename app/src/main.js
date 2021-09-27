96c3674820fafc3776f60431b840290dc0f1301e
义薄云天王某人 <1085240445@qq.com>
2021-09-27 18:39:39 +0800

    引入vue文件

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from '/router'
 Vue.config.productionTip = false
 
 new Vue({
+  router, //将路由实例注册到Vue实例中
   render: h => h(App),
 }).$mount('#app')
---
+++ vue-router/install.js
@@ -0,0 +1,29 @@
+export let _Vue
+
+export default function install(Vue, options) {
+    _Vue = Vue
+        //通过生命周期为所有组件混入router属性
+    Vue.maxin({
+        // 将 new Vue 时传入的 router 实例共享给所有子组件
+        beforeCreate() {
+            if (this.$options.router) { //根组件才有 router
+                this._routerRoot = this //为根组件添加 _routerRoot 属性指向根组件自己
+                this._router = this.$options._router // this._router 指向 this.$options.router
+            } else { // 子组件
+                // 如果是子组件，就去找父亲上的_routerRoot属性，并继续传递给儿子
+                this._routerRoot = this.$options && this.$parent._routerRoot
+            }
+            // 这样，所有组件都能够通过 this._routerRoot._router 获取到同一个 router 实例；
+        }
+    })
+
+    // 注册全局组件：router-link、router-view
+    Vue.component('router-link', {
+        render: h => h('a', {}, '')
+    })
+    Vue.component('router-view', {
+        render: h => h('dev', {}, '')
+    })
+    Vue.prototype.$route = {}
+    Vue.prototype.$router = {}
+}
